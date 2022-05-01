0aee5be3351e0259240219d0469bec0e66ac4d0e
kimgy <akima9a@gmail.com>
2022-05-01 18:53:35 +0900

    220501

--- src/BOJ_4375.java
+++ src/BOJ_4375.java
@@ -1,22 +1,24 @@
-import java.util.Scanner;
+// 문제 URL : https://www.acmicpc.net/problem/4375
+// Math.pow(a,b); a의 b승
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class BOJ_4375 {
     public static void main(String[] args) throws Exception {
-        // 문제 URL : https://www.acmicpc.net/problem/4375
 
-        Scanner sc = new Scanner(System.in);
-
-        while (sc.hasNextInt()) {
-            int reqNum = sc.nextInt();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str = null;
+        while ((str = bf.readLine()) != null) {
+            int inputNum = Integer.parseInt(str);
             double i = 0;
             long tempNum = (long)Math.pow(10, i);
             int cnt = 1;
-            while (tempNum % (long)reqNum != 0) {
+            while (tempNum % (long)inputNum != 0) {
                 i++;
                 tempNum += (long)Math.pow(10, i);
                 cnt++;
             }
-            System.out.println(cnt);   
+            System.out.println(cnt);
         }
     }
 }
