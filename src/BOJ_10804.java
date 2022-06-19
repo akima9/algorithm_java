ae3782350e35f1fe99ca5a544e04c5f1b76e8677
kimgy <akima9a@gmail.com>
2022-06-19 21:32:44 +0900

    220619

---
+++ src/BOJ_10804.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10804 {
+    public static void main(String[] args) throws Exception{
+        int[] card = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 10; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int start = a - 1;
+            int startIndex = a - 1;
+            int end = b -1 ;
+            int temp = 0;
+            int cnt = 0;
+            while (startIndex <= end) {
+                temp = card[start + cnt];
+                card[start + cnt] = card[end - cnt];
+                card[end - cnt] = temp;
+                cnt++;
+                startIndex++;
+            }//end while
+        }//end for
+
+        for (int i = 0; i < card.length; i++) {
+            System.out.println(card[i]);
+        }
+    }
+}
--- src/BOJ_1267.java
+++ src/BOJ_1267.java
@@ -1,5 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_1267 {
     public static void main(String[] args) throws Exception{
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int Y = 0;
+        int M = 0;
+        for (int i = 0; i < N; i++) {
+            int t = Integer.parseInt(st.nextToken());
+            int tempY = t;
+            int tempM = t;
+
+            if (t > 0) {
+                Y += 10;
+                M += 15;
+            }
+
+            while ((tempY - 30) >= 0) {
+                Y += 10;
+                tempY -= 30;
+            }//end while
+
+            while ((tempM - 60) >= 0) {
+                M += 15;
+                tempM -= 60;
+            }//end while
+        }//end for
+
+        StringBuilder sb = new StringBuilder();
+        if (Y > M) {
+            sb.append("M ").append(M);
+        } else if (Y < M) {
+            sb.append("Y ").append(Y);
+        } else {
+            sb.append("Y M ").append(Y);
+        }
+
+        System.out.println(sb.toString());
+
     }
 }
