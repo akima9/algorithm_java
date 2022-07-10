1642044abc08fe5eb4b6c2305773d9dc2ac4e2cc
kimgy <akima9a@gmail.com>
2022-07-10 23:00:45 +0900

    220710

--- src/BOJ_11328.java
+++ src/BOJ_11328.java
@@ -7,8 +7,6 @@ public class BOJ_11328 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String[][] arr = new String[N][2];
-        String result = "Possible";
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String a = st.nextToken();
