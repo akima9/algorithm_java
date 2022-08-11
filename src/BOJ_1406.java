4d2e50ddb8a634835243c38307792d49cfb8687b
kimgy <akima9a@gmail.com>
2022-08-11 22:11:20 +0900

    220811

--- src/BOJ_1406.java
+++ src/BOJ_1406.java
@@ -1,16 +1,55 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class BOJ_1406 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
+        String str = br.readLine();
         int M = Integer.parseInt(br.readLine());
-        
-        for (int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String command = st.nextToken();
+
+        LinkedList<Character> list = new LinkedList();
+        for (int i = 0; i < str.length(); i++) {
+            list.add(str.charAt(i));
         }
+
+        ListIterator<Character> iter = list.listIterator();
+        while (iter.hasNext()) {
+            iter.next();
+        }
+
+        for (int i = 0; i < M; i++) {
+            String[] cmd = br.readLine().split(" ");
+            switch (cmd[0].charAt(0)) {
+                case 'L':
+                    if (iter.hasPrevious()) {
+                        iter.previous();
+                    }
+                    break;
+                case 'D':
+                    if (iter.hasNext()) {
+                        iter.next();
+                    }
+                    break;
+                case 'B':
+                    if (iter.hasPrevious()) {
+                        iter.previous();
+                        iter.remove();
+                    }
+                    break;
+                case 'P':
+                    iter.add(cmd[1].charAt(0));
+                    break;
+            }
+        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (Character chr : list) {
+            bw.write(chr);
+        }
+        bw.flush();
+        bw.close();
     }
 }
